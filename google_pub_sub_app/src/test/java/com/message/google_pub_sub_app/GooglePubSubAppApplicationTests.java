a783eef983961a3128c99bc02cda973b0a3d3b82
neethu.l <neethu.l@TVMATP363396L.ad.infosys.com>
2020-10-13 17:28:29 +0530

    upload all files

---
+++ google_pub_sub_app/src/test/java/com/message/google_pub_sub_app/GooglePubSubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.message.google_pub_sub_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglePubSubAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
